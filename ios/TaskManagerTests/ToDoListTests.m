80df61e62746ab8ebbb92d74667aace3acef1e20
Natalia Braz <naabraz@gmail.com>
2024-01-25 14:41:47 -0300

    chore: rename project

--- ios/TaskManagerTests/ToDoListTests.m
+++ ios/TaskManagerTests/ToDoListTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ToDoListTests : XCTestCase
+@interface TaskManagerTests : XCTestCase
 
 @end
 
-@implementation ToDoListTests
+@implementation TaskManagerTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
