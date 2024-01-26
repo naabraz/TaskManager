4b31d12380518d4498d158fe90d830f6b3e774f6
Natalia Braz <naabraz@gmail.com>
2024-01-25 22:47:47 -0300

    lib: add firebase performance

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,29 @@ import { SafeAreaView, ScrollView, Text, View, Button } from 'react-native';
 
 import crashlytics from '@react-native-firebase/crashlytics';
 import analytics from '@react-native-firebase/analytics';
+import perf from '@react-native-firebase/perf';
+
+async function getRequest(url: string) {
+  // Define the network metric
+  const metric = await perf().newHttpMetric(url, 'GET');
+
+  // Define meta details
+  metric.putAttribute('user', 'abcd');
+
+  // Start the metric
+  await metric.start();
+
+  // Perform a HTTP request and provide response information
+  const response = await fetch(url);
+  metric.setHttpResponseCode(response.status);
+  metric.setResponseContentType(response.headers.get('Content-Type'));
+  metric.setResponsePayloadSize(Number(response.headers.get('Content-Length')));
+
+  // Stop the metric
+  await metric.stop();
+
+  return response.json();
+}
 
 function App(): React.JSX.Element {
   return (
@@ -16,6 +39,14 @@ function App(): React.JSX.Element {
             onPress={async () =>
               await analytics().logEvent('add_task', {
                 name: 'buy bread',
+              })
+            }
+          />
+          <Button
+            title="Perf Request"
+            onPress={() =>
+              getRequest('https://cadiesart.com/api/home/').then(json => {
+                console.log(json);
               })
             }
           />
