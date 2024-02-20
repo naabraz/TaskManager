173d3b2eb501355b790eae3cce57044f21f01897
Natalia Braz <naabraz@gmail.com>
2024-02-19 21:46:48 -0300

    feat: style add task screen

--- src/screens/AddTask/index.tsx
+++ src/screens/AddTask/index.tsx
@@ -1,13 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import * as Styled from './styles';
 
-const AddTask = () => (
-  <Styled.ScrollView contentInsetAdjustmentBehavior="automatic">
+const AddTask = () => {
+  const [name, onChangeName] = useState('');
+  const [description, onChangeDescription] = useState('');
+
+  const onPressSend = () => {
+    console.log('onPressSend');
+  };
+
+  return (
     <Styled.View>
-      <Styled.Text>Add Task Screen</Styled.Text>
+      <Styled.Input
+        onChangeText={onChangeName}
+        value={name}
+        placeholder="Task name"
+      />
+
+      <Styled.Input
+        onChangeText={onChangeDescription}
+        value={description}
+        placeholder="Task description"
+      />
+
+      <Styled.ButtonView>
+        <Styled.Button
+          onPress={onPressSend}
+          title="Add Task"
+          color="black"
+          accessibilityLabel="Add new task"
+        />
+      </Styled.ButtonView>
     </Styled.View>
-  </Styled.ScrollView>
-);
+  );
+};
 
 export default AddTask;
--- src/screens/AddTask/styles.ts
+++ src/screens/AddTask/styles.ts
@@ -1,13 +1,23 @@
 import styled from 'styled-components/native';
 
-export const ScrollView = styled.ScrollView`
-  background-color: linen;
-`;
-
 export const View = styled.View`
-  background-color: papayawhip;
+  display: flex;
+  flex: 1;
+  justify-content: center;
+  margin-horizontal: 24px;
+  margin-vertical: 24px;
 `;
 
-export const Text = styled.Text`
-  color: #bf4f74;
+export const Input = styled.TextInput`
+  padding: 16px;
+  border: solid 1px tomato;
+  border-radius: 8px;
+  margin-bottom: 16px;
 `;
+
+export const ButtonView = styled.View`
+  background-color: tomato;
+  border-radius: 8px;
+`;
+
+export const Button = styled.Button``;
