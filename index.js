25fff493ac54487b3ae92b3e416afc97f06859d2
Natalia Braz <naabraz@gmail.com>
2024-05-01 17:49:55 -0300

    feat: add react native paper provider

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
+import React from 'react';
 import { AppRegistry } from 'react-native';
+import {
+  MD3LightTheme as DefaultTheme,
+  PaperProvider,
+} from 'react-native-paper';
 
 import App from './src/App';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    primary: 'tomato',
+    secondary: 'yellow',
+  },
+};
+
+const Main = () => (
+  <PaperProvider theme={theme}>
+    <App />
+  </PaperProvider>
+);
+
+AppRegistry.registerComponent(appName, () => Main);
