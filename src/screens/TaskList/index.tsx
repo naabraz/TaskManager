25a520987fa11aad9b42891087f603f5fe809639
Natalia Braz <naabraz@gmail.com>
2024-05-01 19:09:31 -0300

    feat: use react native paper on task list

--- src/screens/TaskList/index.tsx
+++ src/screens/TaskList/index.tsx
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { StyleSheet } from 'react-native';
 
 import { useNavigation, ParamListBase } from '@react-navigation/native';
 import { NativeStackNavigationProp } from '@react-navigation/native-stack';
-import { faAdd } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
+import { Card, Text, IconButton, FAB, Divider } from 'react-native-paper';
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
-import {
-  TasksList,
-  TaskContainer,
-  TaskName,
-  TaskDescription,
-  EmptyText,
-  AddTaskButton,
-  RemoveTaskButton,
-  RemoveTaskButtonTitle,
-} from './styles';
+import { TasksList } from './styles';
 import { useTaskStore } from '../../hooks/useStore';
+
+const IconNotCompleted = () => (
+  <MaterialIcons name="radio-button-unchecked" size={24} />
+);
+
+const IconCompleted = () => (
+  <MaterialIcons name="radio-button-checked" size={24} />
+);
+
+const IconDelete = () => <MaterialIcons name="delete" size={24} />;
+
+const CardButtonLeft = (id: string, completed: boolean) => {
+  const changeStatus = useTaskStore(state => state.changeStatus);
+  const Icon = completed ? IconCompleted : IconNotCompleted;
+
+  return <IconButton icon={Icon} onPress={() => changeStatus(id)} />;
+};
+
+const CardButtonRight = (id: string) => {
+  const removeTask = useTaskStore(state => state.removeTask);
+
+  return <IconButton icon={IconDelete} onPress={() => removeTask(id)} />;
+};
 
 const TaskList = () => {
   const tasks = useTaskStore(state => state.tasks);
-  const removeTask = useTaskStore(state => state.removeTask);
-  const changeStatus = useTaskStore(state => state.changeStatus);
+
   const navigation = useNavigation<NativeStackNavigationProp<ParamListBase>>();
 
   const emptyTasksMessage = "Woo hoo! You don't have any tasks!";
@@ -29,35 +43,43 @@ const TaskList = () => {
     navigation.navigate('AddTask');
   };
 
+  const styles = StyleSheet.create({
+    fab: {
+      marginTop: 20,
+    },
+    card: {
+      marginBottom: 20,
+    },
+  });
+
   return (
     <TasksList contentInsetAdjustmentBehavior="automatic">
       {!tasks.length && (
-        <TaskContainer>
-          <EmptyText>{emptyTasksMessage}</EmptyText>
-        </TaskContainer>
+        <>
+          <Card style={styles.card}>
+            <Card.Content>
+              <Text variant="bodyMedium">{emptyTasksMessage}</Text>
+            </Card.Content>
+          </Card>
+        </>
       )}
 
       {tasks.map(task => (
-        <TaskContainer key={task.id} $completed={task.completed}>
-          <TaskName>{task.name}</TaskName>
-          <TaskDescription>{task.description}</TaskDescription>
-          <TaskDescription>
-            {`Completed: ${String(task.completed)}`}
-          </TaskDescription>
-
-          <RemoveTaskButton onPress={() => changeStatus(task.id)}>
-            <RemoveTaskButtonTitle>Complete</RemoveTaskButtonTitle>
-          </RemoveTaskButton>
-
-          <RemoveTaskButton onPress={() => removeTask(task.id)}>
-            <RemoveTaskButtonTitle>Remover</RemoveTaskButtonTitle>
-          </RemoveTaskButton>
-        </TaskContainer>
+        <Fragment key={task.id}>
+          <Card style={styles.card}>
+            <Card.Title
+              title={task.name}
+              subtitle={task.description}
+              left={() => CardButtonLeft(task.id, task.completed)}
+              right={() => CardButtonRight(task.id)}
+            />
+          </Card>
+        </Fragment>
       ))}
 
-      <AddTaskButton onPress={addTask}>
-        <FontAwesomeIcon icon={faAdd} color="white" />
-      </AddTaskButton>
+      <Divider />
+
+      <FAB icon="plus" onPress={addTask} label="Add Task" style={styles.fab} />
     </TasksList>
   );
 };
--- src/screens/TaskList/styles.ts
+++ src/screens/TaskList/styles.ts
@@ -1,50 +1,6 @@
 import styled from 'styled-components/native';
 
 export const TasksList = styled.ScrollView`
-  background-color: gray;
-`;
-
-export const TaskContainer = styled.View<{ $completed?: boolean }>`
-  background-color: ${props => (props.$completed ? 'darkcyan' : 'cyan')};
-  margin-vertical: 14px;
-  margin-horizontal: 24px;
+  background-color: white;
   padding: 24px;
-  border-radius: 8px;
-`;
-
-export const TaskName = styled.Text`
-  margin-bottom: 8px;
-  font-size: 18px;
-  font-weight: bold;
-`;
-
-export const TaskDescription = styled.Text`
-  font-size: 16px;
-`;
-
-export const EmptyText = styled.Text``;
-
-export const AddTaskButton = styled.Pressable`
-  background-color: tomato;
-  padding: 8px;
-  border-radius: 50px;
-  width: 64px;
-  height: 64px;
-  align-items: center;
-  justify-content: center;
-  align-self: center;
-`;
-
-export const RemoveTaskButton = styled.Pressable`
-  background-color: tomato;
-  padding: 16px;
-  align-items: center;
-  border-radius: 16px;
-  justify-content: center;
-  align-self: center;
-`;
-
-export const RemoveTaskButtonTitle = styled.Text`
-  color: white;
-  font-size: 16px;
 `;
