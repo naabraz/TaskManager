e911435319a9b3e45af557a83fc838ee666bb6fc
Natalia Braz <naabraz@gmail.com>
2024-05-01 13:01:25 -0300

    feat: persist data

--- src/hooks/useStore.ts
+++ src/hooks/useStore.ts
@@ -1,11 +1,21 @@
 import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
+import { zustandStorage } from 'src/storage/zustandMMKV';
 
 export interface State {
   tasks: { name: string; description: string }[];
   addTask: (task: { name: string; description: string }) => void;
 }
 
-export const useTaskStore = create<State>(set => ({
-  tasks: [],
-  addTask: task => set(state => ({ tasks: [...state.tasks, task] })),
-}));
+export const useTaskStore = create<State>()(
+  persist(
+    set => ({
+      tasks: [],
+      addTask: task => set(state => ({ tasks: [...state.tasks, task] })),
+    }),
+    {
+      name: 'task-manager-storage',
+      storage: createJSONStorage(() => zustandStorage),
+    },
+  ),
+);
